e2d2435631760d344bd4bcad6d83b6cd0d988e1d
Malshaan Kodithuwakku <kodithuw@ualberta.ca>
2023-09-11 19:48:34 -0600

    final push

--- motivationalBot.py
+++ motivationalBot.py
@@ -1,19 +1,39 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
+import random
+import time
 from twilio.rest import Client
 
+# Your Twilio Account SID and Auth Token
+account_sid = 'AC45bb309f20425417b2f45166e56af60b'
+auth_token = '62c4c7e811c28848c28520583fe96d96'
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['AC21de4d14551de24b359aa63385103704']
-auth_token = os.environ['62c4c7e811c28848c28520583fe96d96']
-client = Client(account_sid, auth_token)
+# Your Twilio phone number and the recipient's phone number
+twilio_phone_number = '+18148697158'
+recipient_phone_number = '+15875329229'
 
-message = client.messages \
-    .create(
-         body='This is the ship that made the Kessel Run in fourteen parsecs?',
-         from_='+18148697158',
-         to='+15875329229'
-     )
+# List of random messages
+random_messages = [
+    'The more you do, the less you wait',
+    'Hey, blow out the candles, make a wish, what is a life if you never take a risk? Aint a place to far aint a dream too big',
+    'You make your mistakes, your mistakes never make you',
+    'It aint your money till you make it, till then its just a conversation',
+    'You miss every shot you dont take',
+]
 
-print(message.sid)
+# Function to send a random message
+def send_random_message():
+    client = Client(account_sid, auth_token)
+    message = random.choice(random_messages)
+    client.messages.create(
+        body=message,
+        from_=twilio_phone_number,
+        to=recipient_phone_number
+    )
+    print(f"Message sent: '{message}'")
+
+# Main loop to send messages at random times
+while True:
+    send_random_message()
+    # Sleep for a random time between 4 to 6 hours 
+    sleep_time = random.randint(14400, 21600)
+    print(f"Next message will be sent in {sleep_time} seconds...")
+    time.sleep(sleep_time)
